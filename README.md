2b189cde16867077e9dfca818bc72763af71dbfc
Bojan Knezevic <bojan.knezevic@gmail.com>
2025-03-11 02:16:23 +0100

    implement modal with titles on bill row, change favicon, update README

--- README.md
+++ README.md
@@ -66,15 +66,16 @@ If a component uses something that is not specific just to itself, then that cod
 
 ## 4 - Tooling and standards
 Tools and standards used in this project are:
-- [Vite](https://vite.dev/) as a tooling solution and bundler.
-- [Vitest](https://vitest.dev/) a performant unit testing framework powered by Vite.
-- [ESLint](https://eslint.org/) to ensure identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs.
-- [Prettier](https://prettier.io/) code formatter to ensure consistent coding style. Some custom formatting rules are applied, as well as imports sorting.
+- [Vite](https://vite.dev/) as a tooling solution and bundler
+- [Vitest](https://vitest.dev/) a performant unit testing framework powered by Vite
+- [ESLint](https://eslint.org/) to ensure identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs
+- [Prettier](https://prettier.io/) code formatter to ensure consistent coding style. Some custom formatting rules are applied, as well as imports sorting
 - [Husky](https://typicode.github.io/husky/) for implementing and executing git hooks to ensure that code with errors doesn't get pushed to the repository. In the case when pushing something that does not need tests or app building, the checks can be skipped by using the `--no-verify` flag, for example ``` git commit -m "commit description" --no-verify ```
   - pre-commit hook is set up to check for linting errors, and run tests
   - pre-push hook is set up to run the project for production and ensure the application builds before pushing to remote origin
-- Absolute imports, to make it easier to import components, hooks, utils, etc, and avoid messy import paths.
-- When developing the application, always use the `dev` branch, and upon completing the code, a pull request should be made for it to be merged into `main` branch.
+- Utilize .env formanaging environment variables
+- Absolute imports, to make it easier to import components, hooks, utils, etc, and avoid messy import paths
+- When developing the application, always use the `dev` branch, and upon completing the code, a pull request should be made for it to be merged into `main` branch
 
 
 ## 5 - API communication
@@ -89,6 +90,14 @@ Filtering uses additional parameter, `bill_status`, and is handled through a mul
 
 ## 6 - State management
 [Zustand](https://zustand-demo.pmnd.rs/) is used for the favourites functionality, implementing a global store to hold the information on favoured bills, and handle adding/removing them, with persisting the state to local storage.
+
+
+## 7 - Styling
+Since the requirement was MaterialUI, styling relies mostly on its own component styling, utilizing only inline styling where necessary.
+
+
+## 8 - 
+
 
 ## Additional considerations
 Some features i would like to have added are snackbars for information on adding or removing a bill from favourites
--- src/components/data-table/components/data-table-row.tsx
+++ src/components/data-table/components/data-table-row.tsx
@@ -1,18 +1,50 @@
+import DOMPurify from 'dompurify';
+
+import { useState } from 'react';
+
 import { Bill } from '@custom-types/bill';
 import StarIcon from '@mui/icons-material/Star';
-import { IconButton, TableCell, TableRow } from '@mui/material';
+import { Box, IconButton, Modal, Tab, TableCell, TableRow, Tabs, Typography } from '@mui/material';
 import useFavouritesStore from '@stores/favourite-bills-store';
 
 interface BillRowProps {
   bill: Bill;
 }
 
+const modalBoxStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  bgcolor: '#ffffff',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const DataTableRow = ({ bill }: BillRowProps) => {
+  const [modalOpened, setModalOpened] = useState(false);
+  const [currentTab, setCurrentTab] = useState(0);
   const isFavourite = useFavouritesStore((state) => state.isFavourite(bill.shortTitleEn));
   const addFavourite = useFavouritesStore((state) => state.addFavourite);
   const removeFavourite = useFavouritesStore((state) => state.removeFavourite);
 
-  const handleToggleFavourite = () => {
+  const handleToggleModal = (state: boolean) => {
+    setModalOpened(state);
+  };
+
+  const handleTabChange = (_event: React.SyntheticEvent, value: number) => {
+    setCurrentTab(value);
+  };
+
+  const sanitizeHTML = (str: string) => {
+    return DOMPurify.sanitize(str);
+  };
+
+  const handleToggleFavourite = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.stopPropagation();
+
     if (isFavourite) {
       removeFavourite(bill);
     } else {
@@ -21,33 +53,57 @@ const DataTableRow = ({ bill }: BillRowProps) => {
   };
 
   return (
-    <TableRow hover sx={{ cursor: 'pointer' }}>
-      <TableCell>{bill.shortTitleEn}</TableCell>
-      <TableCell>{bill.billNo}</TableCell>
-      <TableCell>{bill.billType}</TableCell>
-      <TableCell>{bill.status}</TableCell>
-      {/* Note: Presumably the API is still in progress, as some of the sponsors are duplicated, and some probably unnecessary conditions had to be done. */}
-      {/* Note: Also, since in the assignment says sponsor, and the API returns an array of sponsors, here I am displaying only the primary sponsor. */}
-      <TableCell>
-        {bill.sponsors.length
-          ? bill.sponsors
-              .filter(({ sponsor }) => sponsor.isPrimary)
-              .slice(0, 1)
-              .map(({ sponsor }) => {
-                if (sponsor.isPrimary) {
-                  const showAs = sponsor.as.showAs ? sponsor.as.showAs : sponsor.by.showAs;
+    <>
+      <TableRow hover sx={{ cursor: 'pointer' }} onClick={() => handleToggleModal(true)}>
+        <TableCell>{bill.shortTitleEn}</TableCell>
+        <TableCell>{bill.billNo}</TableCell>
+        <TableCell>{bill.billType}</TableCell>
+        <TableCell>{bill.status}</TableCell>
+        {/* Note: Presumably the API is still in progress, as some of the sponsors are duplicated, and some probably unnecessary conditions had to be done. */}
+        {/* Note: Also, since in the assignment says sponsor, and the API returns an array of sponsors, here I am displaying only the primary sponsor. */}
+        <TableCell>
+          {bill.sponsors.length
+            ? bill.sponsors
+                .filter(({ sponsor }) => sponsor.isPrimary)
+                .slice(0, 1)
+                .map(({ sponsor }) => {
+                  if (sponsor.isPrimary) {
+                    const showAs = sponsor.as.showAs ? sponsor.as.showAs : sponsor.by.showAs;
 
-                  return showAs;
-                }
-              })
-          : ''}
-      </TableCell>
-      <TableCell>
-        <IconButton onClick={handleToggleFavourite}>
-          <StarIcon sx={{ color: isFavourite ? 'gold' : 'gray' }} />
-        </IconButton>
-      </TableCell>
-    </TableRow>
+                    return showAs;
+                  }
+                })
+            : ''}
+        </TableCell>
+        <TableCell>
+          <IconButton onClick={handleToggleFavourite}>
+            <StarIcon sx={{ color: isFavourite ? 'gold' : 'gray' }} />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+      <Modal
+        open={modalOpened}
+        onClose={() => handleToggleModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalBoxStyle}>
+          <Tabs value={currentTab} onChange={handleTabChange} sx={{ marginBottom: 4 }}>
+            <Tab label="English" />
+            <Tab label="Gaeilge" />
+          </Tabs>
+
+          <Typography
+            component="div"
+            dangerouslySetInnerHTML={{
+              __html:
+                currentTab === 0 ? sanitizeHTML(bill.longTitleEn) : sanitizeHTML(bill.longTitleGa),
+            }}
+          />
+        </Box>
+      </Modal>
+    </>
   );
 };
 
