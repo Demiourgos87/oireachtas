196ba0bfa98b1c5f34f25530f331ac69c6dc2157
Bojan Knezevic <bojan.knezevic@gmail.com>
2025-03-23 00:24:59 +0100

    implement persistant filters store

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import DataTable from '@components/data-table/data-table';
 import Intro from '@components/intro/intro';
 import { Box, Container } from '@mui/material';
 
-// import TestComponent from '@components/test-component/test-component';
-
 function App() {
   return (
     <Container>
       <Box>
-        {/* <TestComponent /> */}
         <Intro />
         <DataTable />
       </Box>
